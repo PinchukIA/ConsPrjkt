563f8d33a344635d9b55bb67338a4c7f32c39dc6
Igor Pinchuk <iigorpinchuk@yandex.ru>
2026-02-28 14:46:27 +0400

    The Jagdpanther projectile mass parameter has been changed. The formula has been corrected.

--- ConsPrjkt/ConsPrjkt.cpp
+++ ConsPrjkt/ConsPrjkt.cpp
@@ -5,21 +5,21 @@
 #include <cmath>
 
 
-double UnregularRechnung(int pzFront, int pzBord, int pzHinten, int pzGeschwindigkeit, int pzKaliber, double entfernung)
+double UnregularRechnung(int pzFront, int pzBord, int pzHinten, int pzGeschwindigkeit, double pzMasse, int pzKaliber, double entfernung)
 {
-    double doPzFront = static_cast<double>(pzFront) / 100; // Дециметры
-    double doPzBoard = static_cast<double>(pzBord) / 100; // Дециметры
-    double doPzHinten = static_cast<double>(pzHinten) / 100; // Дециметры
+    double doPzFront = static_cast<double>(pzFront) / 100.0; // Дециметры
+    double doPzBoard = static_cast<double>(pzBord) / 100.0; // Дециметры
+    double doPzHinten = static_cast<double>(pzHinten) / 100.0; // Дециметры
     double doPzGeschwindigkeit = static_cast<double>(pzGeschwindigkeit); // Метры в секунду
-    double doPzKaliber = static_cast<double>(pzKaliber) / 100; // Дециметры
+    double doPzKaliber = static_cast<double>(pzKaliber) / 100.0; // Дециметры
 
     double treffer = doPzGeschwindigkeit * pow(2000.0 / (2000.0 + entfernung), 0.28); // Скорость встречи снаряда с целью
 
+    double deMarre = pow(treffer / 2000.0, 1.43) * (pow(pzMasse, 0.71) / pow(doPzKaliber, 1.07));
 
-
-    return 0;
+    return deMarre;
 }
-double RegularRechnung(int pzFront, int pzBord, int pzHinten, int pzGeschwindigkeit, int pzKaliber, double entfernung)
+double RegularRechnung(int pzFront, int pzBord, int pzHinten, int pzGeschwindigkeit, int pzMasse, int pzKaliber, double entfernung)
 {
     double doPzFront = static_cast<double>(pzFront) / 100; // Дециметры
     double doPzBoard = static_cast<double>(pzBord) / 100; // Дециметры
@@ -30,8 +30,9 @@ double RegularRechnung(int pzFront, int pzBord, int pzHinten, int pzGeschwindigk
 
     double treffer = doPzGeschwindigkeit * pow(base, 0.65); // Скорость встречи снаряда с целью
 
+    double deMarre = pow(treffer / 2000.0, 1.43) * (pow(pzMasse, 0.71) / pow(doPzKaliber, 1.07));
 
-    return 0;
+    return deMarre;
 }
 
 int main()
@@ -45,7 +46,8 @@ int main()
     
     getline(iFile, line);
     string pzName;
-    int pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzMasse, pzKaliber;
+    int pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzKaliber;
+    double pzMasse;
 
     cout << "Gib mir nummer des Tanks (1 - 30): ";
     cin >> PzNum;
@@ -63,7 +65,7 @@ int main()
     getline(ss, cell, ','); pzBord = stoi(cell);
     getline(ss, cell, ','); pzHinten = stoi(cell);
     getline(ss, cell, ','); pzGeschwindigkeit = stoi(cell);
-    getline(ss, cell, ','); pzMasse = stoi(cell);
+    getline(ss, cell, ','); pzMasse = stod(cell);
     getline(ss, cell, ','); pzKaliber = stoi(cell);
 
     cout << "Tank: " << pzName << endl;
@@ -79,12 +81,12 @@ int main()
     cin >> entfernung;
     
     if (pzName == "Pz.Kpfw. V Panther" || pzName == "Jagdpanther") {
-        cout << "Zum Zeitpunkt der Kollision hat das Geschoss eine Geschwindigkeit von " << UnregularRechnung(pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzKaliber, entfernung) << " m/c" << endl;
+        
+        cout << "\nAntwort nach der Formel de Marra: " << UnregularRechnung(pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzMasse, pzKaliber, entfernung) << endl;
     }
     else
     {
-        cout << "Geschoss hat " << RegularRechnung(pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzKaliber, entfernung) 
-            << " m/c Geschwindigkeit" << endl;
+        cout << "\nAntwort nach der Formel de Marra: " << UnregularRechnung(pzFront, pzBord, pzHinten, pzGeschwindigkeit, pzMasse, pzKaliber, entfernung) << endl;
     }
 
 
